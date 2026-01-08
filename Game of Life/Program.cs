ef8e82fce1627267db4f4b4b19e6e8d364b2bcf3
Olaf Loewe <olafloewe95@gmail.com>
2026-01-08 18:36:08 +0100

    simulation working

--- Game of Life/Program.cs
+++ Game of Life/Program.cs
@@ -1,21 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
 
 namespace Game_of_Life {
     internal class Program {
         static void Main(string[] args) {
 
-            Generation gen = new Generation(25,25,0.5);
-            
+            Generation gen = new Generation(50,100,0.5);
+            Console.WindowHeight = Console.LargestWindowHeight;
+            Console.WindowWidth = Console.LargestWindowWidth;
+
             for (int i = 0; i < 1000; i ++) {
                 gen.Next();
                 Thread.Sleep(100);
-                Console.Clear();
-
+                Console.SetCursorPosition(0,0);
             }
 
             Game game = new Game();
